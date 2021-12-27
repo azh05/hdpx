460e838f33c27e6a8cee4f2af6c8dcda504f40ad
liumolm <liumolm@gmail.com>
2021-12-27 10:09:52 +0800

    Rename arguments: noise -> low confidence moderate -> moderate confidence to be consistent with the changes in mSigHdp

--- R/interpret_components.R
+++ R/interpret_components.R
@@ -1,14 +1,14 @@
 #' Extract components and exposures from multiple posterior sample chains.
 #'
-#' This function returns components in which we have high confidence, moderate confidence, and little confidence
+#' This function returns components in which we have high confidence, moderate confidence, and low confidence
 #' (likely noise).
 #'
 #' @param multi.chains.retval A list of objects returned from \code{\link[mSigHdp]{CombineChainsAndExtractSigs}}.
 #'
-#' @param confident.prop Components found in >= \code{confident.prop} of
+#' @param high.confidence.prop Components found in >= \code{high.confidence.prop} of
 #'   posterior samples are high confidence components.
 #'
-#' @param noise.prop Components found in < \code{noise.prop} of posterior samples are considered noise components.
+#' @param moderate.confidence.prop Components found in < \code{moderate.confidence.prop} of posterior samples are considered low confidence components.
 #'
 #' @param verbose if TRUE, generate progress messages.
 #'
@@ -16,43 +16,43 @@
 #'  the union of multiple raw clusters of mutations (in the case of mutational
 #'  signature analysis). Invisibly, a list with the following elements: \describe{
 #'
-#' \item{high_confident_components}{A data frame containing the
-#'    components found in >= \code{confident.prop} of posterior samples. Each column is
+#' \item{high_confidence_components}{A data frame containing the
+#'    components found in >= \code{high.confidence.prop} of posterior samples. Each column is
 #'    a component; in the case of mutational signatures the rows are mutation types.}
 #'
-#' \item{high_confident_components_post_number}{A data frame
-#'   in which the first column contains the index of a column in \code{high_confident_components}
+#' \item{high_confidence_components_post_number}{A data frame
+#'   in which the first column contains the index of a column in \code{high_confidence_components}
 #'  and the second column contains the number of posterior samples that
 #'  contributed to that component.}
 #'
-#' \item{high_confident_components_cdc}{A matrix in which each row
+#' \item{high_confidence_components_cdc}{A matrix in which each row
 #'  corresponds to one of the Dirichlet processes, and each column
-#'  corresponds to one component in \code{high_confident_components}.
+#'  corresponds to one component in \code{high_confidence_components}.
 #'  In the case of mutational signature analysis, most of the columns
 #'  correspond to an input biological sample (e.g. individual tumor).}
 #'
-#' \item{moderate_components}{Analogous to \code{high_confident_compents} except for
-#'  components with constituent raw clusters found in >= \code{noise.prop} and
-#'   < \code{confident.prop} posterior samples.}
+#' \item{moderate_confidence_components}{Analogous to \code{high_confidence_compents} except for
+#'  components with constituent raw clusters found in >= \code{moderate.confidence.prop} and
+#'   < \code{high_confidence_prop} posterior samples.}
 #'
-#' \item{moderate_components_post_number}{Analogous to \code{high_confident_components_post_number}.}
+#' \item{moderate_confidence_components_post_number}{Analogous to \code{high_confidence_components_post_number}.}
 #'
-#' \item{moderate_components_cdc}{Analogous to \code{high_confident_components_cdc}.}
+#' \item{moderate_confidence_components_cdc}{Analogous to \code{high_confidence_components_cdc}.}
 #'
-#' \item{noise_components}{Analogous to \code{high_confident_compents} except for
-#'  components with constituent raw clusters found in  < \code{noise.prop} posterior samples.}
+#' \item{low_confidence_components}{Analogous to \code{high_confidence_compents} except for
+#'  components with constituent raw clusters found in  < \code{moderate.confidence.prop} posterior samples.}
 #'
-#' \item{noise_components_post_number}{Analogous to \code{high_confident_components_post_number}.}
+#' \item{low_confidence_components_post_number}{Analogous to \code{high_confidence_components_post_number}.}
 #'
-#' \item{noise_components_cdc}{Analogous to \code{high_confident_components_cdc}.}
+#' \item{low_confidence_components_cdc}{Analogous to \code{high_confidence_components_cdc}.}
 #'
 #' }
 #'
 #' @export
 
 interpret_components <- function(multi.chains.retval,
-                                 confident.prop = 0.90,
-                                 noise.prop = 0.50,
+                                 high.confidence.prop = 0.90,
+                                 moderate.confidence.prop = 0.50,
                                  verbose=T) {
   if (verbose) message("extracting components ", Sys.time())
 
@@ -77,43 +77,43 @@ interpret_components <- function(multi.chains.retval,
   components_cdc <- components_cdc[,order(components_post_number[,2],decreasing=T),drop=F]
   components_post_number <- components_post_number[order(components_post_number[,2],decreasing=T),]
 
-  #the components with more than confident.prop nsamples are
+  #the components with more than high.confidence.prop nsamples are
   #selected as components with high confidence
-  high_confident_components <- components_category_counts[,which(components_post_number[,2]>=(confident.prop*nsamp))]
-  high_confident_components_post_number <- components_post_number[which(components_post_number[,2]>=(confident.prop*nsamp)),]
-  high_confident_components_cdc <- components_cdc[,which(components_post_number[,2]>=(confident.prop*nsamp))]
-  #the components with more than noise.prop nsamples but less
-  #than confident.prop samples are selected as components with
+  high_confidence_components <- components_category_counts[,which(components_post_number[,2]>=(high.confidence.prop*nsamp))]
+  high_confidence_components_post_number <- components_post_number[which(components_post_number[,2]>=(high.confidence.prop*nsamp)),]
+  high_confidence_components_cdc <- components_cdc[,which(components_post_number[,2]>=(high.confidence.prop*nsamp))]
+  #the components with more than moderate.confidence.prop nsamples but less
+  #than confidence.prop samples are selected as components with
   #moderate confidence
-  moderate_components <- components_category_counts[,intersect(which(components_post_number[,2]>=(noise.prop*nsamp)),which(components_post_number[,2]<(confident.prop*nsamp)))]
-  moderate_components_post_number <- components_post_number[intersect(which(components_post_number[,2]>=(noise.prop*nsamp)),which(components_post_number[,2]<(confident.prop*nsamp))),]
-  moderate_components_cdc <- components_cdc[,intersect(which(components_post_number[,2]>=(noise.prop*nsamp)),which(components_post_number[,2]<(confident.prop*nsamp)))]
+  moderate_confidence_components <- components_category_counts[,intersect(which(components_post_number[,2]>=(moderate.confidence.prop*nsamp)),which(components_post_number[,2]<(high.confidence.prop*nsamp)))]
+  moderate_components_post_number <- components_post_number[intersect(which(components_post_number[,2]>=(moderate.confidence.prop*nsamp)),which(components_post_number[,2]<(high.confidence.prop*nsamp))),]
+  moderate_components_cdc <- components_cdc[,intersect(which(components_post_number[,2]>=(moderate.confidence.prop*nsamp)),which(components_post_number[,2]<(high.confidence.prop*nsamp)))]
 
-  #the components with less than noise.prop nsamples
-  #are selected as noise components
-  noise_components <- components_category_counts[,which(components_post_number[,2]<(noise.prop*nsamp))]
-  noise_components_post_number <- components_post_number[which(components_post_number[,2]<(noise.prop*nsamp)),]
-  noise_components_cdc <- components_cdc[,which(components_post_number[,2]<(noise.prop*nsamp))]
+  #the components with less than moderate.confidence.prop nsamples
+  #are selected as low confidence components
+  low_confidence_components <- components_category_counts[,which(components_post_number[,2]<(moderate.confidence.prop*nsamp))]
+  low_confidence_components_post_number <- components_post_number[which(components_post_number[,2]<(moderate.confidence.prop*nsamp)),]
+  low_confidence_components_cdc <- components_cdc[,which(components_post_number[,2]<(moderate.confidence.prop*nsamp))]
 
   #noise components also include components that only occur in one posterior
   #sample of each chain
-  noise_components <- cbind(noise_components,multi.chains.retval$each.chain.noise.components)
+  low_confidence_components <- cbind(low_confidence_components,multi.chains.retval$each.chain.noise.components)
 
-  noise_components_cdc <- cbind(noise_components_cdc,multi.chains.retval$each.chain.noise.cdc)
-
-
+  low_confidence_components_cdc <- cbind(low_confidence_components_cdc,multi.chains.retval$each.chain.noise.cdc)
 
 
-  return(invisible(list(high_confident_components               = high_confident_components,
-                        high_confident_components_post_number   = high_confident_components_post_number,
-                        high_confident_components_cdc           = high_confident_components_cdc,
-                        moderate_components                     = moderate_components,
-                        moderate_components_post_number         = moderate_components_post_number,
-                        moderate_components_cdc                 = moderate_components_cdc,
-                        noise_components                        = noise_components,
-                        noise_components_post_number            = noise_components_post_number,
-                        noise_components_cdc                    = noise_components_cdc
-                        )))
+
+
+  return(invisible(list(high_confidence_components                         = high_confidence_components,
+                        high_confidence_components_post_number             = high_confidence_components_post_number,
+                        high_confidence_components_cdc                     = high_confidence_components_cdc,
+                        moderate_confidence_components                     = moderate_confidence_components,
+                        moderate_confidence_components_post_number         = moderate_confidence_components_post_number,
+                        moderate_confidence_components_cdc                 = moderate_confidence_components_cdc,
+                        low_confidence_components                          = low_confidence_components,
+                        low_confidence_components_post_number              = low_confidence_components_post_number,
+                        low_confidence_components_cdc                      = low_confidence_components_cdc
+  )))
 
 }
 
